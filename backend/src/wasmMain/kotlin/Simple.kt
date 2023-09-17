8a7d0da567185d911cf548eeba60bccd1625eac1
Antonio Masotti <toniomasotti@gmail.com>
2023-09-17 15:25:40 +0200

    little experimentation with wasm

--- backend/src/wasmMain/kotlin/Simple.kt
+++ backend/src/wasmMain/kotlin/Simple.kt
@@ -1,8 +1,19 @@
 import kotlinx.browser.document
 import kotlinx.dom.appendText
+import org.w3c.dom.HTMLElement
 
 fun main() {
-    document.body?.appendText("Hello, ${greet()}!")
+    val element = document.createElement("div").apply {
+        appendText("Hello, ${greet()}!<br>")
+        appendText("Hello Kotlin.. here you are !")
+    }.let(document.body!!::appendChild)
+
+    (element as HTMLElement).style.color = "red"
+    element.style.fontSize = "2em"
 }
 
 fun greet() = "world"
+
+@OptIn(ExperimentalJsExport::class)
+@JsExport
+fun add(a: Int, b: Int) = a + b
