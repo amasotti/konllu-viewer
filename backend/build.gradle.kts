79f9790f8c43b7b1d396106f5cc6f68e33c88c0b
Antonio Masotti <toniomasotti@gmail.com>
2023-09-18 10:15:52 +0200

    remove unneeded files

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -2,7 +2,6 @@ import org.jetbrains.kotlin.gradle.targets.js.dsl.ExperimentalWasmDsl
 
 plugins {
     kotlin("multiplatform") version "1.9.10"
-    id("io.gitlab.arturbosch.detekt") version("1.23.1")
 //    id("com.google.devtools.ksp") version "1.9.10-1.0.13"
 //    id("org.kodein.mock.mockmp") version "1.15.0"
 }
@@ -15,11 +14,6 @@ repositories {
 }
 
 kotlin {
-    jvm {
-        testRuns["test"].executionTask.configure {
-            useJUnitPlatform()
-        }
-    }
     @OptIn(ExperimentalWasmDsl::class)
     wasm {
         binaries.executable()
--- backend/src/wasmMain/resources/myMod.js
+++
@@ -1,6 +0,0 @@
-import myModule from "./KoNLLU-Viewer-wasm.mjs";
-
-console.log("Trying to add 2 and 4")
-console.log(myModule.add(2,4))
-
-export default myModule;
