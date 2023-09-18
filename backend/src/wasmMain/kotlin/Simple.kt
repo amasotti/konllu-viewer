1961e11f1b6022f93bc10e6e76ac4fedc8237a2f
Antonio Masotti <toniomasotti@gmail.com>
2023-09-18 10:16:20 +0200

    simplify simple.kt

--- backend/src/wasmMain/kotlin/Simple.kt
+++ backend/src/wasmMain/kotlin/Simple.kt
@@ -3,17 +3,16 @@ import kotlinx.dom.appendText
 import org.w3c.dom.HTMLElement
 
 fun main() {
-    val element = document.createElement("div").apply {
+
+    document.createElement("div").apply {
         appendText("Hello, ${greet()}!<br>")
         appendText("Hello Kotlin.. here you are !")
     }.let(document.body!!::appendChild)
+//
+//    (element as HTMLElement).style.color = "red"
+//    element.style.fontSize = "2em"
 
-    (element as HTMLElement).style.color = "red"
-    element.style.fontSize = "2em"
 }
 
 fun greet() = "world"
 
-@OptIn(ExperimentalJsExport::class)
-@JsExport
-fun add(a: Int, b: Int) = a + b
