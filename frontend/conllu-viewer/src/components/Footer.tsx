7817fbb3c89049567a5ef975d7c9ae8950238b59
Antonio Masotti <toniomasotti@gmail.com>
2023-09-21 11:13:16 +0200

    setup gh-pages

--- frontend/conllu-viewer/src/components/Footer.tsx
+++ frontend/conllu-viewer/src/components/Footer.tsx
@@ -4,9 +4,9 @@ export function Footer() {
   return (
     <Box as="footer" mt={12}>
       <hr />
-      <Text fontSize=".8em">Created by Toni with ❤️</Text>
+      <Text fontSize=".8em">Created by Toni out of curiosity ❤️</Text>
       <Text fontSize=".8em">
-        <a href="https://github.com/your-github-link" target="_blank" rel="noopener noreferrer">
+        <a href="https://github.com/amasotti/konllu-viewer" target="_blank" rel="noopener noreferrer">
           Code available on GitHub
         </a>
       </Text>
